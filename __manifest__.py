2b5096403e2c8aff2fe274b656eed08c81fcdcf4
onlyone-odoo <matiasb@onlyone.com.ar>
2025-07-11 13:12:25 -0300

    edit manifest depend website_multicompany

--- __manifest__.py
+++ __manifest__.py
@@ -9,13 +9,9 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "17.0.1.0.0",
+    "version": "17.0.2.0.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
-    "depends": ["website"],
+    "depends": ["website", "website_multicompany"],
 }
